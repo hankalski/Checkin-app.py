333105a881b4b1efc1ed9ef6f46710068586dce2
Darya Pavlenko <48902507+hankalski@users.noreply.github.com>
2019-12-08 21:11:26 +0600

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def function_def(x):
-    y = x/math.sin(x)
-
-    if math.sin(x) !=0:
-        return 'Функция выполнима'
+def hello():
+    return "Hello Assertible!"
 
 if __name__ == "__main__":
     app.run()
